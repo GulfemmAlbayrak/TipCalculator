1c4d58796b8984e0cecd4ad75614c9960c81bb64
Gülfem Albayrak <gulfemalbayrak@gmail.com>
2024-06-26 14:28:52 +0300

    Added test case for invalid number of people

--- TipCalculatorTests/SplitBillViewTests.swift
+++ TipCalculatorTests/SplitBillViewTests.swift
@@ -13,16 +13,35 @@ final class SplitBillViewTests: XCTestCase {
         let totalAmount = 100.0
         let numberOfPeople = "4"
         var perPersonAmount = 0.0
+        var showAlert = false
         var splitBillView = SplitBillView(totalAmount: .constant(totalAmount))
         
         let calculation = BillCalculation()
         calculation.calculatePerPersonAmount(totalAmount: totalAmount, numberOfPeople: numberOfPeople) { result, alert in
             perPersonAmount = result
+            showAlert = alert
         }
         
         // Then
         XCTAssertEqual(perPersonAmount, 25.0)
+        XCTAssertFalse(showAlert)
     }
-    
+    func testInvalidNumberOfPeople() {
+        let totalAmount = 100.0
+        let numberOfPeople = "-4"
+        var perPersonAmount = 0.0
+        var showAlert = false
+        var splitBillView = SplitBillView(totalAmount: .constant(totalAmount))
+        
+        let calculation = BillCalculation()
+        calculation.calculatePerPersonAmount(totalAmount: totalAmount, numberOfPeople: numberOfPeople) { result, alert in
+            perPersonAmount = result
+            showAlert = alert
+        }
+        
+        // Then
+        XCTAssertEqual(perPersonAmount, 0.0)
+        XCTAssertTrue(showAlert)
+    }
 
 }
